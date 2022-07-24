2e57d27538fa6ffa8c69eea8ef5b809ad4a67cea
gauravAero <gaurav656ae@gmail.com>
2022-07-24 18:27:32 +0530

    Renamed min_absolute to min_gb, use parameter names

--- allchecks.py
+++ allchecks.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 import shutil
 import sys
-def check_disk_usage(disk, min_absolute, min_percent):
+import os
+
+def check_reboot():
+    return os.path.exists('/run/reboot-required')
+
+def check_disk_usage(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
-    percent_free = du.free / du.total
+    percent_free = 100 * du.free / du.total
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
-        return False
-    return True
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
 
-if not check_disk_usage("/", 2, 10):
-    print('ERROR: Not enough disk space.')
-    sys.exit()
+def main():
+    if check_reboot():
+        print('Pending Reboot.')
+        sys.exit(1)
+    if check_disk_usage(disk="/",min_gb=2,min_percent=10):
+        print('Disk full.')
+        sys.exit(1)
 
-print('Everything OK.')
-sys.exit(0)
+    print('Everything OK.')
+    sys.exit(0)
+
+main()
