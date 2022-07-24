60a1661e4ff914a9c856484a2214b3325e8a578d
GAURAV KUMAR <gaurav656ae@gmail.com>
2022-07-24 17:41:10 +0530

    Update and rename README.md to allchecks.py
    My friend added a new file allchecks.py

--- README.md
+++
@@ -1,8 +0,0 @@
-# health-checks
-Scripts that checks the health of the computers
-
-This repo will be populated with lots of fancy checks.
-
-My friend added a new file to the repo.
-
-Now i want to see what pull command does.
---
+++ allchecks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import shutil
+import sys
+def check_disk_usage(disk, min_absolute, min_percent):
+    du = shutil.disk_usage(disk)
+    percent_free = du.free / du.total
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
+
+if not check_disk_usage("/", 2, 10):
+    print('ERROR: Not enough disk space.')
+    sys.exit()
+
+print('Everything OK.')
+sys.exit(0)
